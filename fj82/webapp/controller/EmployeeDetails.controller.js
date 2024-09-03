1bd1a56546184942215c5de6ba3d8bd954614ed5
fjlork <fj.lorca.bocardo@gmail.com>
2024-09-03 21:40:50 +0000

    conexione_sap

--- fj82/webapp/controller/EmployeeDetails.controller.js
+++ fj82/webapp/controller/EmployeeDetails.controller.js
@@ -1,16 +1,44 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
-], function (Controller, JSONModel, MessageToast) {
-	"use strict";
+    "../model/formatter"
+],function (Controller, formatter) {
 
 	return Controller.extend("lorkgroup.fj82.controller.EmployeeDetails", {
 
 
-		onInit: function () {
+        myFormatter: formatter,
 
-		}
+        onInit: function () {
+            this.oEventBus = sap.ui.getCore().getEventBus();
+        },
+
+
+        onCreateIncidence: function () {
+            console.log("Estoy creando la incidencia");
+            let oTableIncidence = this.getView().byId("tableIncidence");
+            let oNewIncidence = sap.ui.xmlfragment("lorkgroup.fj82.view.fragment.NewIncidence", this);
+            let oIncidenceModel = this.getView().getModel("incidenceModel");
+            let aData = oIncidenceModel.getData();
+            let iIndex = aData.length;
+                aData.push({
+                    number: iIndex + 1
+                });
+                oIncidenceModel.refresh();
+                oNewIncidence.bindElement("incidenceModel>/"+iIndex);
+                oTableIncidence.addContent(oNewIncidence);
+
+        },
+
+        onSaveIncidence: function (oEvent) {
+
+            // let oSource = oEvent.getSource().getParent().getParent();
+            // let oBindingContext = oSource.getBindingContext("incidenceModel");
+            // this.oEventBus.publish("incidence","onSaveODataIncidence",oBindingContext);
+
+            console.log(oSource);
+            // let oModelIncidence = this.getView().getModel("incidenceModel");
+            // console.log(oModelIncidence.getData());
+        }
 
 	});
 });
--- fj82/webapp/controller/Main.controller.js
+++ fj82/webapp/controller/Main.controller.js
@@ -18,7 +18,7 @@ sap.ui.define([
 
              this.oEventBus = sap.ui.getCore().getEventBus();
             this.oEventBus.subscribe("flexible","showEmployeeDetails", this.showEmployeeDetails.bind(this));
-            // this.oEventBus.subscribe("incidence","onSaveODataIncidence", this.onSaveODataIncidence.bind(this))
+            this.oEventBus.subscribe("incidence","onSaveODataIncidence", this.onSaveODataIncidence.bind(this))
 
 
         },
@@ -50,6 +50,42 @@ sap.ui.define([
             let oIncidenceModel = new JSONModel([]);
                 oDetails.setModel(oIncidenceModel, "incidenceModel");
                 oDetails.byId("tableIncidence").removeAllContent();
+
+            },
+
+        //------------------------------------------------------------------------------//
+        //------------------------------------------------------------------------------//
+        //--                 Método:  onSaveODataIncidence                            --//
+        //------------------------------------------------------------------------------//
+        //------------------------------------------------------------------------------//
+            onSaveODataIncidence: function (sChannel, sEventId, oBindingContext) {
+    
+            //     let oDetails = this.getView().byId("details"),
+            //         oBindingContextDetails = oDetails.getBindingContext("northwind");
+    
+            //     // 1. Crear el objecto que contendra los datos a enviar
+            //     let oData = {
+            //         SapId: this.getOwnerComponent().getEmail(),
+            //         EmployeeId: (oBindingContextDetails.getProperty("EmployeeID")).toString(),
+            //         CreationDate: oBindingContext.getProperty("CreationDate"),
+            //         Type: oBindingContext.getProperty("Type"),
+            //         Reason: oBindingContext.getProperty("Reason")
+            //     };
+    
+            //     console.log(oData);
+    
+            //     let oModel = this.getView().getModel("erp13");
+            //     let oModel = this.getOwnerComponent().getModel("erp13");
+            //     let sUrl = "/IncidentsSet";
+    
+            //         oModel.create(sUrl, oData, {
+            //             success: function () {
+            //                 MessageToast.show("OK");
+            //             },
+            //             error: function () {
+            //                 MessageToast.show("Failed");
+            //             }
+            //         });
 
             }
 
